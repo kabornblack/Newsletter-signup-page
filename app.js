c4ec37ea5d812681823255876a9aaf62bb5d3986
kabornblack <shaibu.kabiru@email.com>
2023-03-14 01:18:14 +0200

    Used mailchimp API  to request and post data to mailchimp server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express =  require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -14,11 +15,44 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  console.log(firstName + lastName + email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us21.api.mailchimp.com/3.0/lists/732d69e084";
+
+  const options = {
+    method: "POST",
+    auth: "kabiru:103e5f04713a17c148cacb0e8420cb63-us21"
+  }
+
+  const request = https.request(url, options, function(response){
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
+
 })
+
+
 
 
 
@@ -26,3 +60,7 @@ app.post("/", function(req, res){
 app.listen(3000, function(){
   console.log("server is running at port 3000");
 });
+
+
+// APIkey: 103e5f04713a17c148cacb0e8420cb63-us21
+// list ID: 732d69e084
